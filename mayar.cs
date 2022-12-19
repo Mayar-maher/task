f15c8f9b11331827c1890f5a35385d3e13057593
mayar <mayar.maher521@gmail.com>
2022-12-19 21:54:29 +0200

    new commit

--- mayar.cs
+++ mayar.cs
@@ -1,233 +1,201 @@
+// name:Mayar Maher Abo el-Ala Ahmed .... section:17 .... and i'm sorry for being late.
 using System;
-public class Person{
- private string _name;   
- public string Name
- {
-    get 
+namespace Tasks;
+
+public class Person
+{
+    private string? _Name;
+    private int _Age;
+    public Person(string name, int age)
     {
-        return _name;
-    } 
-    set
-    {
-         if(value == null || value=="" || value.Length>=32)
-    {
-        throw new Exception("invalid name");
-    } 
-        _name=value;
-    }
- }
- private int _Age;
- public Person(string name,int age)
- {
-  if(name == null || name=="" || name.Length>=32)
-  {
-    throw new Exception("invalid name");
-  } 
-  if (age<=0 ||age>=128){
-    Console.WriteLine("invalid age");
-    return;
-  } 
-  Name=name;
-  _Age =age;
- }
- 
- public int Getage() =>_Age;
- 
- 
- public virtual  void Print()
-    {
-      Console.WriteLine($"My name is {Name}, my age is {_Age}");
+        if (name == null || name == "" || name.Length >= 32)
+        {
+            throw new Exception("Wrong name!");
+        }
+        if (age <= 0 || age > 128)
+        {
+            throw new Exception("Wrong age!");
+        }
+        _Name = name!;
+        _Age = age;
     }
 
+    public string GetName() => _Name!;
+
+    public int GetAge() => _Age;
+
+    public void SetName(string name)
+    {
+        _Name = name;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine($"My name : {_Name},and my age : {_Age}");
+    }
 }
 
-public class Student :  Person
+public class Student : Person
 {
-    private int _Year;
-    public int Year
+
+    public int Year;
+    public float Gpa;
+    public Student(string name, int age, int year, float gpa) : base(name, age)
     {
-      get 
-    {
-        return _Year;
-    } 
-    set
-    {
-        _Year=value;
-    }   
-    }
-    private float _Gpa;
-    public float Gpa
-    {
-       get 
-    {
-        return _Gpa;
-    } 
-    set
-    {
-        _Gpa=value;
-    }      
-    }
-    public Student(string name,int age,int year,float gpa ):base(name, age)
-    {
-        if (year<1 ||year>5)
+        if (year < 1 || year > 5)
         {
-            throw new Exception("invalid year");
+            throw new Exception("wrong year!");
         }
-        if(gpa<0 ||gpa>4)
+
+        if (gpa < 0 || gpa > 4)
         {
-            throw new Exception("invalid gpa");
+            throw new Exception("wrong gpa!");
         }
-        Year=year;
-        Gpa=gpa; 
+
+        Year = year;
+        Gpa = gpa;
     }
     public override void Print()
     {
-        Console.WriteLine($"my name is{Name},my age is {Getage()},and my gpa is{Gpa}");
-    }
-}
-public class Stuff :Person 
-{
-    private int _JoinYear;
-    public int JoinYear
-    {
-          get 
-    {
-        return _JoinYear;
-    } 
-    set
-    {
-        _JoinYear=value;
-    }   
-    }
-    private double _Salary;
-    public double Salary
-    {
-          get 
-    {
-        return _Salary;
-    } 
-    set
-    {
-        _Salary=value;
-    }   
-    }
-    public Stuff(string name,int age,int joinyear,double salary ):base(name, age)
-    
-    {
-        if(salary<=0 || salary>120000)
-        {
-           throw new Exception("invalid salary");  
-        }
-        if(joinyear>age+21)
-        {
-             throw new Exception("invalid joinyear");
-        }
-        Salary=salary;
-        JoinYear=joinyear;
-    }
-     public override void Print()
-    {
-        Console.WriteLine($"my name is{Name},my age is {Getage()},and my salary is{Salary}");
+        Console.WriteLine($"My name : {GetName()}, my age : {GetAge()}, my gpa : {Gpa}");
     }
 }
 public class Database
 {
-private int _current =0;   
-public Person [] Peple =new Person [50]; 
-public void Addperson (Person person)
-{
-  // if (_current==49) return; 
-   Peple[_current++]=person; 
-}
-public void Addstudent (Student student)
-{
-  // if (_current==49) return; 
-   Peple[_current++]=student; 
-}
-public void Addstuff (Stuff stuff)
-{
-  // if (_current==49) return; 
-   Peple[_current++]=stuff; 
-}
-public void PrintAll()
-{
-    for(var i = 0 ; i <_current; i++)
+    private int _currentIndex;
+    public Person[] People = new Person[50];
+    public void AddStudent(Student student)
     {
-        Peple[i].Print();
+        People[_currentIndex++] = student;
     }
-  } 
+    public void AddStaff(Staff staff)
+    {
+        People[_currentIndex++] = staff;
+    }
+    public void AddPerson(Person person)
+    {
+        People[_currentIndex++] = person;
+    }
 
+    public void PrintAll()
+    {
+        for (var i = 0; i < _currentIndex; i++)
+        {
+            People[i].Print();
+        }
+    }
 }
 
-public class program{
-    private static void Main() 
+public class Staff : Person
+{
+    public double Salary;
+    public int JoinYear;
+    public Staff(string name, int age, double salary, int joinYear) : base(name, age)
     {
-     Console.Write("enter numberfrom 1 to 4:");
-     var number=Convert.ToInt32( Console.ReadLine());
-     if (number==1)
-     {
-     var database= new Database();
-     Console.Write("name:");
-     var name=Console.ReadLine();
-      Console.Write("age:");
-     var age=Convert.ToInt32( Console.ReadLine());
-      Console.Write("year:");
-     var year=Convert.ToInt32( Console.ReadLine());
-      Console.Write("gpa:");
-     var gpa=Convert.ToSingle( Console.ReadLine());
-     try
-     {
-     Student student=new Student(name,age,year,gpa);
-     database.Addstudent(student);
-     }
-     catch
-     {
-       Console.WriteLine("invalid input"); 
-     }
-     
-     }
-     else if(number==2)
-     {
-     var database= new Database();
-     Console.Write("name:");
-     var name=Console.ReadLine();
-      Console.Write("age:");
-     var age=Convert.ToInt32( Console.ReadLine());
-      Console.Write("joinyear:");
-     var joinyear=Convert.ToInt32( Console.ReadLine());
-      Console.Write("slasry:"); 
-     var salary=Convert.ToDouble( Console.ReadLine());
-     try
-     {
-     var stuff=new Stuff (name,age,joinyear,salary);
-     database.Addstuff(stuff);  
-     }
-     catch
-     {
-        Console.WriteLine("invalid input");      
-     }
-     }
-    else if(number==3)
-    {
-     var database= new Database();
-     Console.Write("name:");
-     var name=Console.ReadLine();
-      Console.Write("age:");
-     var age=Convert.ToInt32( Console.ReadLine());
-     try
-     {
-     var person=new Person (name,age);
-     database.Addperson(person);
+        if (salary < 0 || salary > 120000)
+        {
+            throw new Exception("wrong salary!");
+        }
+
+        if (joinYear - age <= 21)
+        {
+            throw new Exception("wrong join year!");
+        }
+
+        Salary = salary;
+        JoinYear = joinYear;
     }
-    catch(Exception e)
+    public override void Print()
     {
-        Console.WriteLine(e.Message);
-    }
-    }
-    else 
-    {
-      var database= new Database();
-      database.PrintAll();  
+        Console.WriteLine($"My name : {GetName()}, my age : {GetAge()}, my salary : {Salary}");
     }
 }
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var database = new Database();
+
+        while (true)
+        {
+            Console.WriteLine("1.Add Student , 2.Add Staff , 3.Add Person , 4.Print All");
+            Console.Write("Your Option: ");
+
+            var option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.Write("name: ");
+                    var name = Console.ReadLine();
+                    Console.Write("age: ");
+                    var age = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("year: ");
+                    var year = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("gpa: ");
+                    var gpa = Convert.ToSingle(Console.ReadLine());
+
+                    try
+                    {
+                        var student = new Student(name!, age, year, gpa);
+
+                        database.AddStudent(student);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                    }
+                    break;
+
+                case 2:
+                    Console.Write("name: ");
+                    var name2 = Console.ReadLine();
+                    Console.Write("age: ");
+                    var age2 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("salary: ");
+                    var salary = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("join year: ");
+                    var joinYear = Convert.ToInt32(Console.ReadLine());
+
+                    try
+                    {
+                        var staff = new Staff(name2!, age2, salary, joinYear);
+
+                        database.AddStaff(staff);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                    }
+                    break;
+
+                case 3:
+                    Console.Write("name: ");
+                    var name3 = Console.ReadLine();
+                    Console.Write("age: ");
+                    var age3 = Convert.ToInt32(Console.ReadLine());
+
+                    try
+                    {
+                        var person = new Person(name3!, age3);
+
+                        database.AddPerson(person);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                    }
+
+                    break;
+
+                case 4:
+                    database.PrintAll();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter an option (1:4)");
+                    break;
+            }
+        }
+    }
 }
+//thanks
